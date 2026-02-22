2358f5ff5554edd4bca211532651880f17be51e3
l_hs <hans.loong@outlook.com>
2026-02-22 18:04:11 +0800

    u

--- dotfiles/KDE/.config/plasma-workspace/env/path.sh
+++ dotfiles/KDE/.config/plasma-workspace/env/path.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # 添加~/.local/bin到PATH环境变量
 # 不能在environment.d中设置, PATH会被桌面环境覆盖, 改用KDE预启动脚本
-export PATH="$HOME/.local/bin:$PATH"
+append_path(){ case ":$PATH:" in *:"$1":*) ;; *) PATH="${PATH:+$PATH:}$1" ;; esac; }
+append_path "$HOME/.local/bin"
