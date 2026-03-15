9d19064b86c2f0cd38ede8b220ef931bc1326b52
l_hs <hans.loong@outlook.com>
2026-03-15 15:55:39 +0800

    u

--- dotfiles/Fish/.config/fish/config.fish
+++ dotfiles/Fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 function fish_greeting
-    echo \
-        (set_color 7fb3a1)"$USER" \
-        (set_color b6c97a)"awake" \
-        (set_color 6faec3)" " \
-        (set_color 5c8f9e)"@" \
-        (set_color c4c9cd)(date "+%H:%M:%S")
+    # echo \
+    #     (set_color 7fb3a1)"$USER" \
+    #     (set_color b6c97a)"awake" \
+    #     (set_color 6faec3)" " \
+    #     (set_color 5c8f9e)"@" \
+    #     (set_color c4c9cd)(date "+%H:%M:%S")
 end
